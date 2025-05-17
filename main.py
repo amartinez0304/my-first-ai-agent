27da5d53cb26ab5ee74b5cc8d94fa5470a3526b8
arnmartinezs <a.martinez0304@gmail.com>
2025-05-17 22:46:12 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@tool
+def calculator(a:float, b:float) -> str:
+    """Useful for performing basic arithmetic calculation with numbers
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        str: _description_
+    """
+    print("Tool has been called")
+    return f"The sume of {a} and {b} is {a + b}"
+    
+
+def main():
+    model = ChatOpenAI(temperature=0)
+    tools = [calculator]
+    agent_executor = create_react_agent(model, tools)
+    print("Welcome! I am your AI assistant. Type 'quit' to exit.")
+    print("You can ask me to perform calculations or a chat with me.")
+    
+    while True:
+        user_input = input("\nYou: ").strip()
+        if user_input == 'quit':
+            break
+        
+        print("\nAssisstant: ", end= " ")
+        for chunk in agent_executor.stream(
+            {"messages":[HumanMessage(content=user_input)]}
+        ):
+            if "agent" in chunk and "messages" in chunk["agent"]:
+                for message in chunk["agent"]["messages"]:
+                    print(message.content, end="")
+        print()
+        
+if __name__ == "__main__":
+    main()
